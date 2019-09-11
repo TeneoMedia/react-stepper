28f06317f0bfc35a00555d2315a4a32a8107f468
Jon Manzo <manzo@teneomedia.net>
2019-09-11 12:17:06 -0400

    fixes bad update prevent

--- src/step.tsx
+++ src/step.tsx
@@ -15,9 +15,8 @@ const Step: React.FunctionComponent<Props> = ({ stepId, ...props }) => {
 
   React.useEffect(() => {
     createStep(stepId, props);
-
     return () => removeStep(stepId);
-  });
+  }, []);
 
   React.useEffect(() => {
     updateStep(stepId, {
